a1a14752ca5bd99de7a94c6df12c34351d7ba6dc
Kieran Browne <kierantbrowne@gmail.com>
2018-07-24 17:16:21 +1000

    accidental colour experiment

--- sketch.js
+++ sketch.js
@@ -3,13 +3,19 @@ function setup() {
 
     shad = createShader(
             vert(),
-            frag(lantern()));
+            frag(lantern(
+                    mult(slides.empty,
+                         slides.bar))));
+
     shader(shad);
     shad.setUniform("ratio", width/height);
+    // slide 1 rotation matrix
+    shad.setUniform("rot1", rot(0)); // id matrix
+    frameRate(30);
 }
 
 function draw() {
-    shad.setUniform("uTime", frameCount/20.);
+    shad.setUniform("uTime", millis()/1000.);
     // quad(-1,-1,1,-1,1,1,-1,1);
     quad(-1, -1, 1, -1, 1, 1, -1, 1);
 }
@@ -19,9 +25,12 @@ function frag(colour) {
       "varying vec2 vPos;" +
       "uniform float uTime;" +
       "uniform float ratio;" +
+      "mat2 rot1 = mat2(cos(uTime),sin(uTime),-sin(uTime),cos(uTime));" +
+      "mat2 rot2 = mat2(cos(-uTime),sin(-uTime),-sin(-uTime),cos(-uTime));" +
       "void main(){" +
-      "  vec2 p = vPos;" +
-      "  p.x *= ratio;" +
+      "  vec2 uv = vPos;" +
+      "  uv.x *= ratio;" +
+      "  vec2 st = vec2(atan(uv.x,uv.y), length(uv));" +
       "  vec3 colour = " + colour +
       "  gl_FragColor = vec4(colour,1.);" +
       "}";
@@ -37,7 +46,21 @@ function vert(content) {
 
 function lantern(slide="vec3(1.)") {
     return "min(" +
-        "vec3(smoothstep(1.,0.97,distance(p,vec2(0,0))))," +
+        "vec3(smoothstep(1.,0.97,distance(uv,vec2(0,0))))," +
         slide +
         ");"
 }
+
+function rot(deg) {
+    return [cos(deg), sin(deg), -sin(deg), cos(deg)];
+}
+
+function mult(a,b) {
+    return a +"*"+ b;
+}
+
+const slides = { 
+    empty: "vec3(1.)",
+    // bar: "vec3(smoothstep(0.36,0.4,abs((rot2 * p).y)))"
+    bar: "vec3((rot1* st.x)/6.2831 + .5)"
+};
