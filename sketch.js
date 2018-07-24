95f604d0696c7d4f29580f6a0ecbcfccab4cee5c
Kieran Browne <kierantbrowne@gmail.com>
2018-07-24 15:47:38 +1000

    initial

---
+++ sketch.js
@@ -0,0 +1,36 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight, WEBGL);
+
+    shad = createShader(
+            vert(),
+            frag("vec3(smoothstep(1.,0.97,distance(p,vec2(0,0))));"));
+    shader(shad);
+    shad.setUniform("ratio", width/height);
+}
+
+function draw() {
+    shad.setUniform("uTime", frameCount/20.);
+    // quad(-1,-1,1,-1,1,1,-1,1);
+    quad(-1, -1, 1, -1, 1, 1, -1, 1);
+}
+
+function frag(colour) {
+  return "precision highp float;" +
+      "varying vec2 vPos;" +
+      "uniform float uTime;" +
+      "uniform float ratio;" +
+      "void main(){" +
+      "  vec2 p = vPos;" +
+      "  p.x *= ratio;" +
+      "  vec3 colour = " + colour +
+      "  gl_FragColor = vec4(colour,1.);" +
+      "}";
+}
+function vert(content) {
+  return "precision highp float;" +
+      "varying vec2 vPos;" +
+      "attribute vec3 aPosition;" +
+      "void main(){" +
+      "  vPos = (gl_Position = vec4(aPosition, 1.0)).xy;" +
+      "}";
+}
